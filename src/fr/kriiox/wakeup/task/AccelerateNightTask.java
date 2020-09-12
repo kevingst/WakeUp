95eb27bf6c7abfb59727328dbda03b6832cbff04
Kriiox <kevin.gosset25@gmail.com>
2020-09-12 17:47:26 +0200

    Add reset phantom spawn [1.5.1]

--- src/fr/kriiox/wakeup/task/AccelerateNightTask.java
+++ src/fr/kriiox/wakeup/task/AccelerateNightTask.java
@@ -1,7 +1,9 @@
 package fr.kriiox.wakeup.task;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Statistic;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class AccelerateNightTask extends BukkitRunnable {
@@ -15,6 +17,9 @@ public class AccelerateNightTask extends BukkitRunnable {
         if(timeWorld >= (1200 - timeRate * 1.5) && timeWorld <= 1200) {
             if(world.hasStorm()){
                 world.setStorm(false);
+            }
+            for (Player player : world.getPlayers()){
+                player.setStatistic(Statistic.TIME_SINCE_REST, 0);
             }
             this.cancel();
         } else {
